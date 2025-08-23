4a22ecc4b783d5abc78f2e07c15d4be144de75ab
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-08-23 14:42:01 +0700

    Tuan 01: Bai 06

---
+++ Tuan_01/dist/Bai06.js
@@ -0,0 +1,9 @@
+"use strict";
+class Book {
+    constructor(attributes, title, author, year) {
+        this.attributes = attributes;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
---
+++ Tuan_01/src/Bai06.ts
@@ -0,0 +1,14 @@
+class Book{
+    attributes: string;
+    title: string;
+    author: string;
+    year: number;
+
+    constructor(attributes: string, title: string, author: string, year: number){
+        this.attributes = attributes;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+}
