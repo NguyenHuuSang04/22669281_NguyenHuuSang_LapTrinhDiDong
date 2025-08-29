66ddfcbdb3cce7cfeb7395067683f724a2403cf6
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-08-29 08:50:00 +0700

    Tuan 01: Bai 08

---
+++ Tuan_01/dist/Bai08.js
@@ -0,0 +1,22 @@
+"use strict";
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+const products = [
+    new Product("A", 10),
+    new Product("B", 101),
+    new Product("C", 102),
+    new Product("D", 130),
+    new Product("E", 10),
+    new Product("F", 11),
+    new Product("G", 12),
+    new Product("H", 13),
+    new Product("I", 190),
+    new Product("K", 100),
+    new Product("L", 106),
+];
+const proFilter = products.filter(p => p.price > 100);
+console.log(proFilter);
