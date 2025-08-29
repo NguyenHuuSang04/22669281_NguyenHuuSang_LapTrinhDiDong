5fb9a1916b94a496392b8624abf70c9562d916fa
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-08-29 17:48:17 +0700

    Tuan 01: Bai 25

---
+++ Tuan_01/dist/Bai25.js
@@ -0,0 +1,8 @@
+"use strict";
+class Shape_Bai25 {
+    static describe() {
+        console.log("This is static shape!!!");
+    }
+}
+//Test
+Shape_Bai25.describe(); // static thì gọi trực tiếp, không cần new
---
+++ Tuan_01/src/Bai25.ts
@@ -0,0 +1,8 @@
+class Shape_Bai25 {
+    static describe(): void {
+        console.log("This is static shape!!!");
+    }
+}
+
+//Test
+Shape_Bai25.describe(); // static thì gọi trực tiếp, không cần new
