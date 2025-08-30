f2ce021f8e3a310011e2053715e61c08eae8284b
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-08-30 14:24:30 +0700

    Lab 2 _ Tuan 02: Bai 07

---
+++ Lab_02_Tuan_02/dist/Bai_07.js
@@ -0,0 +1,17 @@
+"use strict";
+function simulateTask_07(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Task done after ${time} ms`);
+        }, time);
+    });
+}
+Promise.race([
+    simulateTask_07(2300),
+    simulateTask_07(1200),
+    simulateTask_07(3200),
+])
+    .then(result => {
+    console.log("Task done earliest!!!");
+    console.log(result);
+});
