09e6c8055b3ab13e7c81116cb799716bb69574f1
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-09-01 11:24:43 +0700

    Lab 2 _ Tuan 02: Bai 15

---
+++ Lab_02_Tuan_02/src/Bai_15.ts
@@ -0,0 +1,17 @@
+function bai_15(num: number): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Done: ${num}`);
+            resolve(num);
+        }, 1000);
+    })
+}
+
+async function run_bai15() {
+    const r1 = await bai_15(1);
+    const r2 = await bai_15(2);
+    const r3 = await bai_15(3);
+    console.log("Results: ", r1, r2, r3);
+}
+
+run_bai15();
