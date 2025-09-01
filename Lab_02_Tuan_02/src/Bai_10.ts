48431d765ad86c2b89a771515be442bb09419735
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-09-01 09:55:12 +0700

    Lab 2 _ Tuan 02: Bai 10

---
+++ Lab_02_Tuan_02/src/Bai_10.ts
@@ -0,0 +1,30 @@
+function donePromise(success: boolean): Promise<string> {
+    return new Promise((resolve, reject) => {
+        if (success) resolve("Done");
+        else reject("Fail");
+    });
+}
+// test done: then + finally được thực hiện
+donePromise(true)
+    .then((result) => {
+        console.log(`Then: ${result}`);
+    })
+    .catch((err) => {
+        console.log(`Catch: ${err}`);
+    })
+    .finally(() => {
+        console.log("Finally: Done ( Then + finally )"); // chạy bất kể success hay failure
+
+    });
+
+// catch + finally được thực hiện
+donePromise(false)
+    .then((result) => {
+        console.log(`Then: ${result}`);
+    })
+    .catch((err) => {
+        console.log(`Catch: ${err}`);
+    })
+    .finally(() => {
+        console.log("Finally: Done ( catch + finally )"); // chạy bất kể success hay failure
+    });
