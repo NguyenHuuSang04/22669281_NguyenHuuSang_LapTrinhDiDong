ee74579cf10d1d90133788a9404b8b7354cf4abb
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-09-02 10:25:28 +0700

    Lab 2 _ Tuan 02: Bai 21

---
+++ Lab_02_Tuan_02/dist/Bai_21.js
@@ -0,0 +1,21 @@
+"use strict";
+async function fetchUsersFromAPI() {
+    try {
+        const respone = await fetch("https://6832d64fc3f2222a8cb3da6f.mockapi.io/api/v1/Users");
+        if (!respone.ok) {
+            throw new Error(`HTTP error! Status: ${respone.status}`);
+        }
+        const data = await respone.json();
+        return data;
+    }
+    catch (error) {
+        console.error("Lỗi khi fetch APT: ", error);
+        return [];
+    }
+}
+async function run_bai_21() {
+    console.log("fetching users từ API...");
+    const users = await fetchUsersFromAPI();
+    console.log("Kết quả: ", users);
+}
+run_bai_21();
