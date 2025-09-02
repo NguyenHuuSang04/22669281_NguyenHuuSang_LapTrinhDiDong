9c359c253c51f2da38f31b19157bc30f34c9721b
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-09-02 10:46:32 +0700

    Lab 2 _ Tuan 02: Bai 23

---
+++ Lab_02_Tuan_02/dist/Bai_23.js
@@ -0,0 +1,21 @@
+"use strict";
+async function fetchTodosFromAPI() {
+    try {
+        const respone = await fetch("https://6832d64fc3f2222a8cb3da6f.mockapi.io/api/v1/todoList");
+        if (!respone.ok) {
+            throw new Error(`HTTP error! Status: ${respone.status}`);
+        }
+        const data = await respone.json();
+        const dataFilte = data.filter((todo) => todo.status === true);
+        return dataFilte;
+    }
+    catch (error) {
+        console.log("Có lỗi khi fetch todo", error);
+        return [];
+    }
+}
+async function run_bai_22() {
+    const todos = await fetchTodosFromAPI();
+    console.log("Danh sách việc đã hoàn thành", todos);
+}
+run_bai_22();
